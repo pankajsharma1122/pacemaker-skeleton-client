299a9ac487fa9faca8969820ad157cb917ac7afd
Pankaj Sharma <pankajsharma1122@gmail.com>
2018-01-02 12:57:00 -0500

    Added code for pacemaker client

---
+++ src/main/java/controllers/PacemakerAPI.java
@@ -0,0 +1,391 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import models.Activity;
+import models.Location;
+import models.User;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+interface PacemakerInterface
+{
+    @GET("/users")
+    Call<List<User>> getUsers();
+    
+    @POST("/users")
+    Call<User> registerUser(@Body User User);
+    
+    @POST("/users/{id}/status/{status}")
+    Call<User> updateActivationStatus(@Path("id") String id,@Path("status") String status);
+    @POST("/users/{id}/roles/{role}")
+    Call<User> updateRole(@Path("id") String id,@Path("role") String role);
+    
+    @GET("/users/{id}/activities")
+    Call<List<Activity>> getActivities(@Path("id") String id);
+    
+    @GET("/users/{id}/activities/{sortBy}")
+    Call<List<Activity>> listActivities(@Path("id") String id,@Path("sortBy") String sortBy);
+    
+
+    @POST("/users/{id}/activities")
+    Call<Activity> addActivity(@Path("id") String id, @Body Activity activity);
+    
+    
+    @GET("/users/{email}")
+    Call<User> getUserByEmail(@Path("email") String email);
+    
+    @GET("/users/{id}/activity/{activityId}")
+    Call<Activity> getActivity(@Path("id") String userId, @Path("activityId") String activityId);
+    
+    @GET("/users/{id}/activities/{activityId}/locations")
+    Call<List<Location>> getActivityLocations(@Path("id") String id,@Path("activityId") String activityId);
+    
+    @POST("/users/{id}/activities/{activityId}/locations")
+    Call<Location> addLocation(@Path("id") String id,@Path("activityId") String activityId, @Body Location location);
+    
+    @POST("/users/{id}/friends/{friendId}/follow")
+    Call<User> followAFriend(@Path("id") String id,@Path("friendId") String friendId);
+    @POST("/users/{id}/friends/{friendId}/unfollow")
+    Call<User> unfollowAFriend(@Path("id") String id,@Path("friendId") String friendId);
+    
+    @GET("/users/{id}/friends")
+    Call<List<User>> listAllFriends(@Path("id") String id);
+    
+    @DELETE("/users")
+    Call<User> deleteUsers();
+
+    @DELETE("/users/{id}")
+    Call<User> deleteUser(@Path("id") String id);
+
+    @GET("/users/{id}")
+    Call<User> getUser(@Path("id") String id);
+    
+    @DELETE("/users/{id}/activities")
+    Call<String> deleteActivities(@Path("id") String id);
+    
+    @GET("/users/{id}/activities/{activityId}/locations")
+    Call<List<Location>> getLocations(@Path("id") String id, @Path("activityId") String activityId);
+    
+    
+    @POST("/users/{id}/friends/{friendEmail}/message")
+    Call<User> messageAFriend(@Path("id") String userId,@Path("friendEmail") String friendEmail, @Body String message);
+    @POST("/users/{id}/friends/message")
+    Call<User> messageAllFriend(@Path("id") String userId, @Body String message);
+    @GET("/users/{id}/messages")
+    Call<List<String>> listAllMessages(@Path("id") String id);
+}
+
+public class PacemakerAPI {
+
+  PacemakerInterface pacemakerInterface;
+
+  public PacemakerAPI(String url) {
+    Gson gson = new GsonBuilder().create();
+    Retrofit retrofit = new Retrofit.Builder().baseUrl(url)
+        .addConverterFactory(GsonConverterFactory.create(gson)).build();
+    pacemakerInterface = retrofit.create(PacemakerInterface.class);
+  }
+
+  public Collection<User> getUsers() {
+    Collection<User> users = null;
+    try {
+      Call<List<User>> call = pacemakerInterface.getUsers();
+      Response<List<User>> response = call.execute();
+      users = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return users;
+  }
+
+  public void deleteUsers() {
+    try {
+      Call<User> call = pacemakerInterface.deleteUsers();
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public User createUser(String firstName, String lastName, String email, String password) {
+    User returnedUser = null;
+    try {
+      Call<User> call = pacemakerInterface.registerUser(new User(firstName, lastName, email, password));
+      Response<User> response = call.execute();
+      returnedUser = response.body();    
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return returnedUser;
+  }
+
+  public Activity createActivity(String id, String type, String location, double distance) {
+    Activity returnedActivity = null;
+    try {
+      Call<Activity> call = pacemakerInterface.addActivity(id, new Activity(type, location, distance));
+      Response<Activity> response = call.execute();
+      returnedActivity = response.body();    
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return returnedActivity;
+  }
+
+  public Activity getActivity(String userId, String activityId) {
+    Activity activity = null;
+     try {
+       Call<Activity> call = pacemakerInterface.getActivity(userId, activityId);
+       Response<Activity> response = call.execute();
+       activity = response.body();
+     } catch (Exception e) {
+       e.printStackTrace();
+     }
+     return activity;
+   }
+
+  public Collection<Activity> getActivities(String id) {
+    Collection<Activity> activities = null;
+    try {
+      Call<List<Activity>> call = pacemakerInterface.getActivities(id);
+      Response<List<Activity>> response = call.execute();
+      activities = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return activities;
+  }
+
+  public List<Activity> listActivities(String userId, String sortBy) {
+    List<Activity> activities = null;
+    try {
+      Call<List<Activity>> call = pacemakerInterface.listActivities(userId, sortBy);
+      Response<List<Activity>> response = call.execute();
+      activities = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return activities;
+  }
+
+  public void addLocation(String id,String activityId, double latitude, double longitude) {
+    
+    try {
+      Call<Location> call = pacemakerInterface.addLocation(id, activityId, new Location(latitude, longitude));
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public User getUserByEmail(String email) {
+    Collection<User> users = getUsers();
+    User foundUser = null;
+    for (User user : users) {
+      if (user.email.equals(email)) {
+        foundUser = user;
+      }
+    }
+    return foundUser;
+  }
+
+  public User getUser(String id) {
+    User user = null;
+    try {
+      Call<User> call = pacemakerInterface.getUser(id);
+      Response<User> response = call.execute();
+      user = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return user;
+  }
+
+  public User deleteUser(String userId) {
+    User user = null;
+    try {
+      Call<User> call = pacemakerInterface.deleteUser(userId);
+      Response<User> response = call.execute();
+      user = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return user;
+  }
+
+  public List<Location> getLocations(String id, String activityId) {
+    List<Location> locations = null;
+    try {
+      Call<List<Location>> call = pacemakerInterface.getLocations(id, activityId);
+      Response<List<Location>> response = call.execute();
+      locations = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return locations;
+  }
+  
+  public void deleteActivities(String id) {
+    try {
+      Call<String> call = pacemakerInterface.deleteActivities(id);
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public void followAFriend(String userEmail, String friendEmail)
+  {
+    try {
+      Call call = pacemakerInterface.followAFriend(userEmail,friendEmail);
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    
+  }
+  
+  public void unfollowAFriend(String userEmail, String friendEmail)
+  {
+    try {
+      Call call = pacemakerInterface.unfollowAFriend(userEmail,friendEmail);
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    
+  }
+  
+  public void messageAFriend(String userId, String friendEmail, String message)
+  {
+    try {
+      Call call = pacemakerInterface.messageAFriend(userId,friendEmail,message);
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+  
+  public void messageAllFriend(String userId,String message)
+  {
+    try {
+      Call call = pacemakerInterface.messageAllFriend(userId,message);
+      call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+  public Collection<String> listAllMessages(String userId)
+  {
+    List<String> messages = null;
+    try {
+      Call<List<String>> call = pacemakerInterface.listAllMessages(userId);
+      Response<List<String>> response = call.execute();
+      messages = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return messages;
+    
+  }
+  
+  public Collection<User> listAllFriends(String userId)
+  {
+    List<User> friends = null;
+    try {
+      Call<List<User>> call = pacemakerInterface.listAllFriends(userId);
+      Response<List<User>> response = call.execute();
+      friends= response.body();
+      //response<List<User> call.execute();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return friends;
+  }
+
+  public Collection<Activity> friendActivityReport(String id,String friendEmail)
+  {
+    Collection<Activity> activities = null;
+    Collection<User> friends =  listAllFriends(id);
+    Collection<User> selectedFriends = 
+        friends.stream().filter(
+            user -> user.email.equalsIgnoreCase(friendEmail)).collect(Collectors.toList());
+    Optional<User> selectedFriend = selectedFriends.stream().findFirst();
+    if (selectedFriend.isPresent()) {
+      User user = selectedFriend.get();
+      activities = listActivities(user.getId(),"type");
+    } else {
+      System.out.println("Provided Email is not in friend List");
+    }
+    return activities;
+    
+  }
+
+  public List<Activity> distanceLeaderBoard(String id)
+  {
+    Collection<User> friends =  listAllFriends(id);
+    List<Activity> allFriendsActivities = new ArrayList<Activity>();
+    friends.forEach(user -> allFriendsActivities.addAll(getActivities(user.getId())));
+    allFriendsActivities.sort((a1, a2) -> Double.compare(a2.distance,a1.distance));
+    return allFriendsActivities;
+  }
+
+  public Collection<Activity> distanceLeaderBoardByType(String id, String type)
+  {
+    List<Activity> allFriendsActivities = distanceLeaderBoard(id);
+    List<Activity> allFriendsActivitiesByType = allFriendsActivities.stream().filter(activity -> 
+        activity.getType().equalsIgnoreCase(type)).collect(Collectors.toList());
+    return allFriendsActivitiesByType;
+  }
+
+  public Collection<Activity> locationLeaderBoard(String id, String location)
+  {
+    List<Activity> allFriendsActivities = distanceLeaderBoard(id);
+    List<Activity> allFriendsActivitiesByLocation = allFriendsActivities.stream().filter(activity -> 
+    activity.getLocation().equalsIgnoreCase(location)).collect(Collectors.toList());
+    return allFriendsActivitiesByLocation;
+  }
+
+  public User setActivationStatus(String id, String status)
+  {
+    User user = null;
+    try {
+      Call<User> call = pacemakerInterface.updateActivationStatus(id,status);
+      Response<User> response = call.execute();
+      user = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return user;
+  }
+  
+  public User setUserRole(String id, String role)
+  {
+    User user = null;
+    try {
+      Call<User> call = pacemakerInterface.updateRole(id,role);
+      Response<User> response = call.execute();
+      user = response.body();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return user;
+  }
+
+  
+}
---
+++ src/main/java/controllers/PacemakerConsoleService.java
@@ -0,0 +1,303 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.base.Optional;
+
+import asg.cliche.Command;
+import asg.cliche.Param;
+import models.Activity;
+import models.User;
+import parsers.AsciiTableParser;
+import parsers.Parser;
+
+public class PacemakerConsoleService {
+
+  private PacemakerAPI paceApi = new PacemakerAPI("https://warm-escarpment-62674.herokuapp.com/");
+  //https://warm-escarpment-62674.herokuapp.com/
+  //http://localhost:7000
+  private Parser console = new AsciiTableParser();
+  private User loggedInUser = null;
+
+  public PacemakerConsoleService() {
+  }
+
+  // Starter Commands
+
+  @Command(description = "Register: Create an account for a new user")
+  public void register(@Param(name = "first name") String firstName,
+      @Param(name = "last name") String lastName,
+      @Param(name = "email") String email, @Param(name = "password") String password) {
+    
+    console.renderUser(paceApi.createUser(firstName, lastName, email, password));
+  }
+  @Command(description = "Register: Create an account for a new user")
+  public void deleteUsers() {
+    if(!isUserLoggedIn()) {return;}
+    if(!isUserAdmin()) {return;}
+    paceApi.deleteUsers();
+    logout();
+  }
+  
+  @Command(description = "Register: Create an account for a new user")
+  public void deleteUser(@Param(name = "userId") String userId) {
+    if(!isUserLoggedIn()) {return;}
+    if(!isUserAdmin()) {return;}
+    paceApi.deleteUser(userId);
+  }
+
+  @Command(description = "List Users: List all users emails, first and last names")
+  public void listUsers() {
+    console.renderUsers(paceApi.getUsers());
+  }
+
+  @Command(description = "Login: Log in a registered user in to pacemaker")
+  public void login(@Param(name = "email") String email,
+      @Param(name = "password") String password) {
+    
+    Optional<User> user = Optional.fromNullable(paceApi.getUserByEmail(email));
+    if (user.isPresent()) {
+      if (user.get().password.equals(password) && user.get().getEnabled()) {
+        loggedInUser = user.get();
+        console.println(loggedInUser.toString());
+        console.println("ok");
+      } else {
+        console.println("Error on login.");
+        if(!user.get().getEnabled()) {
+          console.println("USER is disabled.");
+        }
+        console.println(user.get().toString());
+        
+      }
+    }
+  }
+
+  @Command(description = "Logout: Logout current user")
+  public void logout() {
+    if (null != loggedInUser) {
+      console.println("Logging out " + loggedInUser.email);
+      console.println("ok");
+    }
+    
+    loggedInUser = null;
+  }
+
+  @Command(description = "Add activity: create and add an activity for the logged in user")
+  public void addActivity(
+      @Param(name = "type") String type,
+      @Param(name = "location") String location,
+      @Param(name = "distance") double distance) {
+    if(!isUserLoggedIn()) {return;}
+    Optional<User> user = Optional.fromNullable(loggedInUser);
+    if (user.isPresent()) {
+      console.renderActivity(paceApi.createActivity(user.get().id, type, location, distance));
+    }
+  }
+
+  
+  
+  @Command(description = "List Activities: List all activities for logged in user")
+  public void listLocation(@Param(name = "activity-id") String activityId) {
+    Optional<User> user = Optional.fromNullable(loggedInUser);
+    if (user.isPresent()) {
+      console.renderLocations(paceApi.getLocations(user.get().id, activityId));
+    }
+  }
+
+  // Baseline Commands
+
+  @Command(description = "Add location: Append location to an activity")
+  public void addLocation(@Param(name = "activity-id") String activityId,
+      @Param(name = "longitude") double longitude,
+      @Param(name = "latitude") double latitude) {
+    if(!isUserLoggedIn()) {return;}
+    Optional<Activity> activity = Optional.fromNullable(paceApi.getActivity(loggedInUser.getId(), activityId));
+    if (activity.isPresent()) {
+      paceApi.addLocation(loggedInUser.getId(),activity.get().id, latitude, longitude);
+      console.println("ok");
+    } else {
+      console.println("Activity not found");
+    }
+  }
+
+  @Command(description = "List Activities: List all activities for logged in user")
+  public void listActivities() {
+    Optional<User> user = Optional.fromNullable(loggedInUser);
+    if (user.isPresent()) {
+      console.renderActivities(paceApi.getActivities(user.get().id));
+    }
+  }
+  
+  @Command(description = "ActivityReport: List all activities for logged in user, sorted alphabetically by type")
+  public void activityReport() {
+    if(!isUserLoggedIn()) {return;}
+    Optional<User> user = Optional.fromNullable(loggedInUser);
+    if (user.isPresent()) {
+      System.out.println("USER :: " + user.get().id);
+      console.renderActivities(paceApi.listActivities(user.get().id, "type"));
+    }
+  }
+
+  @Command(
+      description = "Activity Report: List all activities for logged in user by type. Sorted longest to shortest distance")
+  public void activityReport(@Param(name = "byType: type") String type) {
+    if(!isUserLoggedIn()) {return;}
+    Optional<User> user = Optional.fromNullable(loggedInUser);
+    if (user.isPresent()) {
+      List<Activity> reportActivities = new ArrayList<>();
+      Collection<Activity> usersActivities = paceApi.listActivities(user.get().id, "DISTANCE");
+      usersActivities.forEach(a -> {
+        if (a.type.equals(type))
+          reportActivities.add(a);
+      });
+      reportActivities.sort((a1, a2) -> {
+        if (a1.distance >= a2.distance)
+          return -1;
+        else
+          return 1;
+      });
+      console.renderActivities(reportActivities);
+    }
+  }
+
+  @Command(description="List all locations for a specific activity",name="listActivityLocations", abbrev="lal")
+  public void listActivityLocations(@Param(name = "activity-id") String id) {
+    if(!isUserLoggedIn()) {return;}
+    Optional<Activity> activity = Optional.fromNullable(paceApi.getActivity(loggedInUser.getId(), id));
+    if (activity.isPresent()) {
+      console.renderLocations(paceApi.getLocations(id, activity.get().id));
+    }
+  }
+
+  @Command(description = "Follow Friend: Follow a specific friend")
+  public void follow(@Param(name = "email") String email) {
+    if(!isUserLoggedIn()) {return;}
+    paceApi.followAFriend(loggedInUser.getEmail(),email);
+  }
+  
+  @Command(description = "Unfollow Friends: Stop following a friend")
+  public void unfollowFriend(@Param(name = "email") String email) {
+    if(!isUserLoggedIn()) {return;}
+    paceApi.unfollowAFriend(loggedInUser.getEmail(),email);
+  }
+
+  @Command(description = "List Friends: List all of the friends of the logged in user")
+  public void listFriends() {
+    if(!isUserLoggedIn()) {return;}
+    System.out.println("********Friends *******");
+    console.renderUsers(paceApi.listAllFriends(loggedInUser.getId()));
+  }
+
+  @Command(description = "Friend Activity Report: List all activities of specific friend, sorted alphabetically by type)")
+  public void friendActivityReport(@Param(name = "email") String friendEmail) {
+    if(!isUserLoggedIn()) {return;}
+    console.renderActivities(paceApi.friendActivityReport(loggedInUser.getId(),friendEmail));
+  }
+
+  // Good Commands
+
+  
+
+  @Command(description = "Message Friend: send a message to a friend")
+  public void messageFriend(@Param(name = "email") String friendEmail,
+      @Param(name = "message") String message) {
+    if(!isUserLoggedIn()) {return;}
+    paceApi.messageAFriend(loggedInUser.getId(), friendEmail, message);
+  }
+
+  @Command(description = "List Messages: List all messages for the logged in user")
+  public void listMessages() {
+    if(!isUserLoggedIn()) {return;}
+    console.renderMessageList(paceApi.listAllMessages(loggedInUser.getId()));
+  }
+
+  @Command(description = "Distance Leader Board: list summary distances of all friends, sorted longest to shortest")
+  public void distanceLeaderBoard() {
+    if(!isUserLoggedIn()) {return;}
+    console.renderActivities(paceApi.distanceLeaderBoard(loggedInUser.getId()));
+  }
+
+  // Excellent Commands
+
+  @Command(description = "Distance Leader Board: distance leader board refined by type")
+  public void distanceLeaderBoardByType(@Param(name = "byType: type") String type) {
+    if(!isUserLoggedIn()) {return;}
+    console.renderActivities(paceApi.distanceLeaderBoardByType(loggedInUser.getId(),type));
+  }
+
+  @Command(description = "Message All Friends: send a message to all friends")
+  public void messageAllFriends(@Param(name = "message") String message) {
+    if(!isUserLoggedIn()) {return;}
+    paceApi.messageAllFriend(loggedInUser.getId(), message);
+  }
+
+  @Command(description = "Location Leader Board: list sorted summary distances of all friends in named location")
+  public void locationLeaderBoard(@Param(name = "location") String location) {
+    if(!isUserLoggedIn()) {return;}
+    console.renderActivities(paceApi.locationLeaderBoard(loggedInUser.getId(),location));
+  }
+  @Command(description = "Change user status to enabled or disabled by a Admin user")
+  public void setActivationStatus(@Param(name = "id") String userId,@Param(name = "status") String status) {
+    if(!isUserLoggedIn()) {return;}
+    if(!isUserAdmin()) {return;}
+    if (userId.equalsIgnoreCase(loggedInUser.getId())) {
+      System.out.println("Cannot change self status");
+      return;
+    }
+    if (!status.equalsIgnoreCase("Y") && !status.equalsIgnoreCase("N")) {
+      System.out.println("Please provode valid activation status (Y/N)");
+      return;
+    }
+    paceApi.setActivationStatus(userId,status);
+  }
+  
+  @Command(description = "Change user status to enabled or disabled by a Admin user")
+  public void setUserRole(@Param(name = "id") String userId,@Param(name = "status") String role) {
+    if(!isUserLoggedIn()) {return;}
+    if(!isUserAdmin()) {return;}
+    if(!isValidRoleType(role)) {return;}
+    if (userId.equalsIgnoreCase(loggedInUser.getId())) {
+      System.out.println("Cannot change self status");
+      return;
+    }
+    paceApi.setUserRole(userId,role);
+  }
+  
+  private boolean isValidRoleType(String role)
+  {
+    if ("ADMIN".equals(role) || "USER".equals(role)) {
+      return true;
+    } else {
+      System.out.println("Please pass valid user role (ADMIN/USER)");
+      return false;
+      
+    }
+    
+  }
+
+  private boolean isUserLoggedIn()
+  {
+    if (null == loggedInUser) {
+      System.out.println("User not loged in");
+      return false;
+    } else {
+      System.out.println("User logged in :: >>" + loggedInUser.getEmail() + "::ROLE::" + loggedInUser.getRole());
+      return true;
+    }
+  }
+  private boolean isUserAdmin()
+  {
+    if (!loggedInUser.getRole().equals("ADMIN")) {
+      System.out.println("User not loged in as Administrator");
+      return false;
+    } else {
+      //System.out.println("Admin User logged in :: >>" + loggedInUser.getEmail());
+      return true;
+    }
+  }
+  // Outstanding Commands
+
+  // Todo
+}
